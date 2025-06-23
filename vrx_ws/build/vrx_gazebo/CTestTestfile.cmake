eff815ef07951a88c71a68d1b179982d42bd8608
maximus <chandran.k.2009@gmail.com>
2025-06-23 12:10:29 +0530

    Putting the full vrx workspace here

---
+++ vrx_ws/build/vrx_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chandran/vrx_ws/src/vrx-2.3.3/vrx_urdf/vrx_gazebo
+# Build directory: /home/chandran/vrx_ws/build/vrx_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
